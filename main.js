291c11b85b4fdf6cbee5cbc57c7b2eb03a792e8b
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2017-07-05 22:34:25 -0400

    Game restart functionality added

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var SCROLL_SPEED = 7;
 var prev_tile_num = 0;
 
 var trex;
+var game_over = false;
 
 $(document).ready( function() {
 	init_game();
@@ -18,16 +19,18 @@ function init_game() {
 
 	ground_init_tiles();
 	clouds_init();
+	obstacles_init();
 	trex = new Player();
 
 	document.body.addEventListener("keydown", ekey_down);
+	document.body.addEventListener("keydown", game_restart);
 	document.body.addEventListener("keyup", ekey_up);
-	
-	GameScrolling();
+
+	game_scrolling();
 }
 
 	
-function GameScrolling() {
+function game_scrolling() {
 	var canvas = document.getElementById("trex_game");
 	var context = canvas.getContext("2d");
 	GAME_POSITION += SCROLL_SPEED;
@@ -37,37 +40,49 @@ function GameScrolling() {
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	context.save();
 	context.translate(-GAME_POSITION, 0);
-	//context.globalCompositeOperation='destination-over';
 	// update
 	ground_tiles_update();
 	clouds_update();
 	trex.update();
+	game_over = obstacles_update();
 
 	// draw
 	ground_tiles_draw(context, canvas);
 	clouds_draw(context);
+	obstacles_draw(context);
 	trex.draw(context);
-	
 
 	context.restore();
-	requestAnimationFrame(GameScrolling);
+	if (game_over) {
+		// text score draw
+		context.fillStyle = "#545659";
+		context.font = "50px Roboto Mono";
+		var font_len = context.measureText("Game Over!").width
+		context.fillText("Game Over!", canvas.width/2 - (font_len / 2), 90);
+
+		context.fillStyle = "#545659";
+		context.font = "20px Roboto Mono";
+		font_len = context.measureText("Score: " + GAME_POSITION).width
+		context.fillText("Score: " + (GAME_POSITION - GAME_WIDTH), canvas.width/2 - (font_len / 2), 180);
+
+		context.fillStyle = "#545659";
+		context.font = "25px Roboto Mono";
+		font_len = context.measureText("SPACE to restart").width
+		context.fillText("SPACE to restart", canvas.width/2 - (font_len / 2), 240);
+	} else {
+		requestAnimationFrame(game_scrolling);
+	}
 }
 
 
-
-
-/*
-context.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh)
-
-img	Source image object	Sprite sheet
-sx	Source x	Frame index times frame width
-sy	Source y	0
-sw	Source width	Frame width
-sh	Source height	Frame height
-dx	Destination x	0
-dy	Destination y	0
-dw	Destination width	Frame width
-dh	Destination height	Frame height
-
-
-*/
+function game_restart(event) {
+	if ((event.keyCode == 32) && (game_over)) {
+		obstacles = [];
+		clouds = [];
+		game_floor_tiles = [];
+		GAME_POSITION = 0;
+		prev_tile_num = 0;
+		init_game();
+		game_over = false;
+	}
+}
